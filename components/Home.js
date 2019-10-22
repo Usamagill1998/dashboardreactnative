06c37418d5131a41071940d367fabd5581104cdc
Usamagill1998 <41861946+Usamagill1998@users.noreply.github.com>
2019-10-22 11:46:57 -0700

    Your Message

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import { StyleSheet, View ,WebView,ImageBackground,AsyncStorage} from 'react-native';
+import { StyleSheet, View ,WebView,ImageBackground,AsyncStorage,Dimensions,Text} from 'react-native';
 import MenuButton from '../components/MenuButton'
 //import MapView from 'react-native-maps';
 //import {MapView}from 'expo';
 import {Image,Tooltip, registerCustomIconType} from 'react-native-elements'
-import { Container, Header, Left, Body, Right, Button, Icon, Title, Text,Content,Card, CardItem} from 'native-base';
-
+import { Container, Header, Left, Body, Right,  Title,Content, CardItem,Icon} from 'native-base';
+import { Card, ListItem, Button} from 'react-native-elements'
 
 
 //import MenuButton from '../components/MenuButton'
@@ -14,7 +14,7 @@ import BgImage from '../assets/background.jpg';
 import { Input} from 'react-native-elements';
 import { TextField } from 'react-native-materialui-textfield'; 
 import ImagePickerExample from '../components/Image';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 
 
 
@@ -34,35 +34,41 @@ export default class LinksScreen extends React.Component {
   }
    )
   }
+  onLayout(e) {
+    const {nativeEvent: {layout: {height}}} = e;
+    this.height = height;
+    this.forceUpdate();
+  }
 
   render() {
-    
-
+    const screenHeight = Dimensions.get('window').height
+    const {height: heightOfDeviceScreen} = Dimensions.get('window');
     return (
-      <View style={{flex:1}}>
-        <View style={{flex:0.1}}>
-      <Container >
-              <Header style={{backgroundColor:'white'}}>
-                
-                  <Left>
-                  <MenuButton navigation={this.props.navigation}/>
-              </Left>                  
-                <Body>
-                <Image
-          style={{width: 50, height: 50,borderRadius:50/2}}
-          source={require('../assets/IMG-20191013-WA0001.jpg')}
-        />
-                </Body>
-                <Right>
-                  <Button transparent>
-                    <Text>ShakeitOff</Text>
-                  </Button>
-                </Right>
-              </Header>
-            </Container>
-            </View>
-            <View style={{flex:0.1,marginTop:5}}>
-            <Container >
+      <View style={styles.container}>
+          <ScrollView>
+<View style={{flex:0.2}}>
+
+      <Header style={{backgroundColor:'white'}}>
+        
+          <Left>
+          <MenuButton navigation={this.props.navigation}/>
+      </Left>                  
+        <Body>
+        <Image
+  style={{width: 50, height: 50,borderRadius:50/2}}
+  source={require('../assets/IMG-20191013-WA0001.jpg')}
+/>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Text>ShakeitOff</Text>
+          </Button>
+        </Right>
+      </Header>
+    
+    </View>
+   
+    <View style={{flex:0.1,marginTop:5}}>
         <Header style={{backgroundColor:'black'}}>
          
           <Body style={{marginLeft:110,paddingBottom:8}}>
@@ -74,212 +80,341 @@ export default class LinksScreen extends React.Component {
             </Button>
           </Right>
         </Header>
-      </Container>
       </View>
       
-      <View style={{flex:0.1}}>
+
+      <View style={{flex:0.2}}>
         <View style={{flex:1,flexDirection:'row'}}>
 
         <View style={{flex:0.6}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem style={{backgroundColor:'grey'}} header button onPress={() => alert("This is Card Header")}>
-              <Text style={{color:'white'}}>Total Sale</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
       </View>
       <View style={{flex:0.4}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem style={{backgroundColor:'grey'}} header button onPress={() => alert("This is Card Header")}>
-              <Text style={{color:'white'}}>3852</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
-      </View>
-
-      </View>
-      </View>
-
-      
-      <View style={{flex:0.1}}>
-        <View style={{flex:1,flexDirection:'row'}}>
-
-        <View style={{flex:0.6}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>Total Bills</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
-      </View>
-      <View style={{flex:0.4}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>21</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
-      </View>
-
-      </View>
-      </View>
-      
-      <View style={{flex:0.1}}>
-        <View style={{flex:1,flexDirection:'row'}}>
-   <View style={{flex:0.6}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>Total Item</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
-      </View>
-      <View style={{flex:0.4}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>35</Text>
-            </CardItem>
-           
-          </Card>
-        </Content>
-      </Container>
-      </View>
-
-      </View>
-      </View>
-         <View style={{flex:0.1}}>
-      <Container >
-        <Header style={{backgroundColor:'orange',marginTop:8}}>
-          <Left/>
-          <Body style={{}}>
-            <Title style={{color:'white',marginBottom:19}}>Activity</Title>
-          </Body>
-          <Right />
-        </Header>
-      </Container>
-      </View>
        
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+     
       <View style={{flex:0.2}}>
         <View style={{flex:1,flexDirection:'row'}}>
 
-        <View style={{flex:0.5}}>
-      <Container >
-        
-        <Content padder>
-          <Card >
-            <CardItem style={{backgroundColor:'grey'}} header button onPress={() => alert("This is Card Header")}>
-              <Text style={{color:'white'}}>First Bill</Text>
-            </CardItem>
-            <CardItem style={{backgroundColor:'grey'}} body button onPress={() => alert("This is Card Footer")}>
-              <Text style={{color:'white'}}>11:37</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
       </View>
-      <View style={{flex:0.5}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem style={{backgroundColor:'grey'}} header button onPress={() => alert("This is Card Header")}>
-              <Text style={{color:'white'}}>Last Bill</Text>
-            </CardItem>
-            
-            <CardItem style={{backgroundColor:'grey'}} body button onPress={() => alert("This is Card Footer")}>
-              <Text style={{color:'white'}}>10:52 pm</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
-      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
+
+
+</View>
 
       </View>
       </View>
 
       <View style={{flex:0.1}}>
-      <Container >
-        <Header style={{backgroundColor:'red'}}>
-          <Left/>
-          <Body style={{}}>
-            <Title style={{color:'white',marginBottom:14}}>Top Selling</Title>
-          </Body>
-          <Right />
-        </Header>
-      </Container>
-      </View>
-      
-      <View style={{flex:0.2}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Top Selling</Text>
+         
+      </Card>
+</View>
+<View style={{flex:0.2}}>
         <View style={{flex:1,flexDirection:'row'}}>
 
         <View style={{flex:0.5}}>
-      <Container >
-        
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>Food</Text>
-            </CardItem>
-            <CardItem body button onPress={() => alert("This is Card Footer")}>
-              <Text>Maagi</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
+        <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Food</Text>
+          <Text style={styles.cardTitle}>Maagi</Text>
+         
+      </Card>
       </View>
+
       <View style={{flex:0.5}}>
-      <Container >
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Food</Text>
+          <Text style={styles.cardTitle}>Maagi</Text>
+         
+      </Card>
+      </View>
+      </View>
+      </View>
+
+<View style={{flex:0.1}}>
+
+
+<Card style={styles.cardStyle}>
+    <Text style={styles.cardTitle}>Tax Total</Text>
+   
+</Card>
+</View>
+     
+<View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>CGST</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>)0.41</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+     
+      <View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>No tax</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>0.00</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+
+     
+      <View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>SGST</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>0.99</Text>
+         
+      </Card>
+     
+      </View>
+      </View>
+</View>
+     
+<View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Tax</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+      <View style={{flex:0.4}}>
+
+
+<Card style={styles.cardStyle}>
+    <Text style={styles.cardTitle}>Stock Total</Text>
+   
+</Card>
+</View>
+
+<View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Purchases</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+      <View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Transfers</Text> 
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>First Card</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+      <View style={{flex:0.4}}>
+
+
+<Card style={styles.cardStyle}>
+    <Text style={styles.cardTitle}>Stock Total</Text>
+   
+</Card>
+</View>
+
+<View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Purchases</Text>
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>Sale Totals</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+      <View style={{flex:0.2}}>
+        <View style={{flex:1,flexDirection:'row'}}>
+
+        <View style={{flex:0.6}}>
+
+
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>SubTotal</Text> 
+         
+      </Card>
+      </View>
+      <View style={{flex:0.4}}>
+       
+       
+      <Card style={styles.cardStyle}>
+          <Text style={styles.cardTitle}>..</Text>
+         
+      </Card>
+
+
+</View>
+
+      </View>
+      </View>
+
+     </ScrollView>
+
+  </View>
+);
+}
+}
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+//paddingTop: 50,
+},
+cardStyle: {
+//  flex: 0.1,
+margin: 5,
+},
+cardTitle:{
+//padding:10,
+fontSize:20,
+alignItems: "center",
+justifyContent: "center"
+}
+});
+;
         
-        <Content padder>
-          <Card>
-            <CardItem header button onPress={() => alert("This is Card Header")}>
-              <Text>Qyantity:6</Text>
-            </CardItem>
-            
-            <CardItem body button onPress={() => alert("This is Card Footer")}>
-              <Text>Amount:6000</Text>
-            </CardItem>
-          </Card>
-        </Content>
-      </Container>
-      </View>
-
-      </View>
-      </View>
-
-
-            </View>
-          );
-        }
-      }
+      
        
 
           //<View style={{backgroundColor:'black',flex:1}}> 
